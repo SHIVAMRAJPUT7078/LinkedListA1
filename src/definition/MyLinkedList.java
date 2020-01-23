2a6760a1200a75938bf1c0cf974c15bbfda46cda
shivam rajput <shivam.rajput_cs18@gmail.com>
2020-01-23 11:11:32 +0530

    create method addFirst

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -4,6 +4,8 @@ import adt.LinkedListADT;
 
 public class MyLinkedList<E> implements LinkedListADT<E> {
     private static class Node<E> {
+        private Node<E> head = null;
+        private int size = 0;
         private E data;
         private Node<E> next;
 
@@ -23,11 +25,17 @@ public class MyLinkedList<E> implements LinkedListADT<E> {
         private Node(E data) {
             this.data = data;
         }
+
+        private void addFirst(E item) {
+            Node<E> newNode = new Node<>(item);
+            head = new Node<>(item, head);
+            size++;
+        }
     }
+
 
     @Override
     public void add(int index, E item) {
-
     }
 }
 
